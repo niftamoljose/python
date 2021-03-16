85ad6c021daeeb56fafc8b20e10f4e9070012cc6
niftamoljose <74651803+niftamoljose@users.noreply.github.com>
2021-03-16 15:56:18 +0530

    added

---
+++ rectangle.py
@@ -0,0 +1,14 @@
+class rectangle:
+    def __init__ (self, length, breadth, area):
+        self.length = length
+        self.breadth = breadth
+        self.area = area
+    def arearectangle(self):
+        area= length*breadth
+    def displayrectangle(self):
+        print("length=", self.length)
+        print("breadth=", self.breadth)
+        print("area=", self.area)
+
+rect1=rectangle(2,3,6 )
+rect1. displayrectangle()
